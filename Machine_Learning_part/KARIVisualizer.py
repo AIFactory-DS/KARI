be8f80c861467106c13611341df4eb25592f4109
yoosunyoung <luysunyoung9@gmail.com>
2021-08-10 13:37:18 +0900

    Visualize base anchor boxes

--- Machine_Learning_part/KARIVisualizer.py
+++ Machine_Learning_part/KARIVisualizer.py
@@ -4,9 +4,9 @@ import os
 import random
 
 
-def sample_image_visualization():
-    output_path = 'data/sample_images'
-    image_path = 'data/processed.npy'
+def visualize_sample_image():
+    output_path = '../data/sample_images'
+    image_path = '../data/processed.npy'
     image_list = np.load(image_path, allow_pickle=True)
     image_list = image_list[()]['train']
     image_name_list = list(image_list.keys())[:5]
@@ -24,5 +24,23 @@ def sample_image_visualization():
         cv2.imwrite(os.path.join(output_path, image_name), image)
 
 
+def visualize_anchor_box(image_size=(150, 150), anchor_scale=[4, 8, 16, 32, 64], anchor_ratio=[1, 1/3, 3]):
+    from FasterRCNN import generate_base_anchor_boxes
+    anchor_boxes = generate_base_anchor_boxes(image_size=image_size, anchor_ratio=anchor_ratio, anchor_scale=anchor_scale)
+    import numpy as np
+    blank_image = np.zeros((image_size[0], image_size[1]), np.uint8)
+    cx = image_size[0]/2
+    cy = image_size[1]/2
+    for ib, box in enumerate(anchor_boxes):
+        x1 = int(cx+box[0]*image_size[0])
+        x2 = int(cx+box[2]*image_size[0])
+        y1 = int(cy+box[1]*image_size[1])
+        y2 = int(cy+box[3]*image_size[1])
+        blank_image = cv2.rectangle(blank_image, (x1, y1), (x2, y2), (int(ib//3)*100, int(ib//3)*100, 255), 1)
+    cv2.imshow("base anchor box", blank_image)
+    cv2.waitKey(0)
+
+
 if __name__ == "__main__":
-    sample_image_visualization()
+    # visualize_sample_image()
+    visualize_anchor_box(image_size=(400, 400), anchor_ratio=[1, 2, 1/2], anchor_scale=[32, 64, 128])
